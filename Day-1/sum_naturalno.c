fc1a4669c3b9201432fcd8be2af1dcfb878f5ffa
Sakshi <sakshiy492007@gmail.com>
2026-05-28 21:41:37 +0530

    Day-1 uploaded.

---
+++ Day-1/sum_naturalno.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n,sum=0, i;
+    printf("Enter the natural number:");
+    scanf("%d", &n);
+    for (i = 1; i<=n; ++i) 
+        {
+            sum += i;
+        }
+    printf("The sum of first %d  natural numbers is %d.", n,sum);
+    return 0;        
+}
